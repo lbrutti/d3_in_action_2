352792cff4766a47e1c56f12aa461028f0b4c643
Lorenzo Brutti <lorenzo.brutti@ekr.it>
2019-01-04 01:43:00 +0100

    inserito colori icone

--- chapter3/soccerviz.js
+++ chapter3/soccerviz.js
@@ -33,19 +33,48 @@ function overallTeamViz(incomingData) {
     .attr("y", 30)
     .text(d => d.team);
 
-    d3.text("../resources/modal.html", html => {
-      d3.select("body")
-        .append("div")
-        .attr("id", "modal")
-        .html(html);
-    });
+  d3.text("../resources/modal.html", html => {
+    d3.select("body")
+      .append("div")
+      .attr("id", "modal")
+      .html(html);
+  });
   teamG.on("click", teamClick);
-  function teamClick(d){
+
+  function teamClick(d) {
     d3.selectAll("td.data").data(d3.values(d))
       .html(p => p);
   }
-  d3.selectAll("g.overallG").insert("image", "text")
-    .attr("xlink:href", d => `../images/${d.team}.png`)
-    .attr("width", "45px").attr("height", "20px").attr("x", "-22")
-    .attr("y", "-10");
+
+  //carico icona
+  d3.html("../resources/icon.svg", loadSVG);
+
+  function loadSVG(svgData) {
+    d3.selectAll("g.overallG").each(function() {
+      let gParent = this;
+      d3.select(svgData).selectAll("path").each(function() {
+        gParent.appendChild(this.cloneNode(true));
+      });
+    });
+
+    d3.selectAll("g.overallG").each(function(d) {
+      d3.select(this).selectAll("path").datum(d);
+    });
+
+    var fourColorScale = d3.scaleOrdinal()
+      .domain(["UEFA", "CONMEBOL", "CONCACAF", "AFC"])
+      .range(["#5eafc6", "#FE9922", "#93C464", "#fcbc34"]);
+
+    d3.selectAll("path")
+      .style("fill", p => {
+        if (p) {
+          return fourColorScale(p.region);
+        }
+      })
+      .style("stroke", "black").style("stroke-width", "1px");
+  }
+  // d3.selectAll("g.overallG").insert("image", "text")
+  //   .attr("xlink:href", d => `../images/${d.team}.png`)
+  //   .attr("width", "45px").attr("height", "20px").attr("x", "-22")
+  //   .attr("y", "-50");
 }
