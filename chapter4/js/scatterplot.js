a0010ff6835b6837353883150b1d10f1bfd27bd5
Lorenzo Brutti <lorenzo.brutti@ekr.it>
2019-01-12 00:19:25 +0100

    finito scatterplot

--- chapter4/js/scatterplot.js
+++ chapter4/js/scatterplot.js
@@ -1,29 +1,61 @@
 /*jshint esversion:6 */
-(function(){
-  function dataviz(){
-    var scatterData = [
-      {friends: 5, salary: 22000},
-      {friends: 3, salary: 18000},
-      {friends: 10, salary: 88000},
-      {friends: 0, salary: 180000},
-      {friends: 27, salary: 56000},
-      {friends: 8, salary: 74000}
+(function() {
+  function dataviz() {
+    var scatterData = [{
+        friends: 5,
+        salary: 22000
+      },
+      {
+        friends: 3,
+        salary: 18000
+      },
+      {
+        friends: 10,
+        salary: 88000
+      },
+      {
+        friends: 0,
+        salary: 180000
+      },
+      {
+        friends: 27,
+        salary: 56000
+      },
+      {
+        friends: 8,
+        salary: 74000
+      }
     ];
 
-    var xExtent = d3.extent(scatterData, function(d) { return d.salary; });
-    var yExtent = d3.extent(scatterData, function(d) { return d.friends; });
+    //calcolo il dominio dei dati
+    var xMax = d3.max(scatterData, function(d) {
+      return d.salary;
+    });
+    var yMax = d3.max(scatterData, function(d) {
+      return d.friends;
+    });
+    //funzioni per calcolo della scala
     var xScale = d3.scaleLinear()
-        .domain(xExtent)
-        .range([0, 500]);
+      .domain([0, xMax])
+      .range([0, 500]);
     var yScale = d3.scaleLinear()
-        .domain(yExtent)
-        .range([0, 500]);
+      .domain([0, yMax])
+      .range([0, 500]);
+
+    //sezione per disegno assi
+    //right e bottom si riferiscono alla posizione delle label e tick
+    var yAxis = d3.axisRight().scale(yScale).tickSize(500).ticks(16);
+    var xAxis = d3.axisBottom().scale(xScale).tickSize(500).ticks(4);
+    d3.select("svg").append("g").attr("id", "yAxisG").call(yAxis);
+    d3.select("svg").append("g").attr("id", "xAxisG").call(xAxis);
+    // d3.selectAll("#xAxisG").attr("transform", "translate(0,500)");
+
     d3.select("svg").selectAll("circle")
-    .data(scatterData).enter()
-    .append("circle")
-    .attr("r", 5)
-    .attr("cx", (d,i) => xScale(d.salary))
-    .attr("cy", d => yScale(d.friends));
+      .data(scatterData).enter()
+      .append("circle")
+      .attr("r", 5)
+      .attr("cx", (d, i) => xScale(d.salary))
+      .attr("cy", d => yScale(d.friends));
   }
   return dataviz();
 })();
