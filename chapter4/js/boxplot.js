2db066e0f2ba06b0390c6d4633890e5b556d6a97
Lorenzo Brutti <lorenzo.brutti@ekr.it>
2019-01-26 00:03:38 +0100

    completato boxplot

--- chapter4/js/boxplot.js
+++ chapter4/js/boxplot.js
@@ -23,16 +23,7 @@ function scatterplot(data) {
       .attr("transform", `translate(0,${tickSize+10})`)
       .attr("id", "xAxisG")
       .call(xAxis);
-
-  d3.select("svg").selectAll("circle.median")
-    .data(data)
-    .enter()
-    .append("circle")
-      .attr("class", "tweets")
-      .attr("r", 5)
-      .attr("cx", d => xScale(d.day))
-      .attr("cy", d => yScale(d.median))
-      .style("fill", "darkgray");
+d3.select("#xAxisG > path.domain").style('display', 'none');
 
   //inizio ad creare elementi del boxplot:
   d3.select("svg")
@@ -42,18 +33,64 @@ function scatterplot(data) {
     .append('g')
     .attr("class", "box")
     //il transform di "g" influenza il posizionamento degli elementi
-    // figli: le loro altezze partodno dal valore mediano
+    // figli: le loro altezze partono dal valore mediano
     .attr('transform', d=> `translate(${xScale(d.day)}, ${yScale(d.median)})`)
+    // .append("rect")
+    // .attr('width', '20')
+    // .attr('x', '0')
+    // .style('stroke', 'black')
+    // .attr('height', 10)
     .each(function(d,i){
+      // line.range
+      d3.select(this)
+        .append("line")
+        .attr("class", "range")
+        .attr('x1', 0)
+        .attr('x2', 0)
+        .attr('y1', yScale(d.max) - yScale(d.median))
+        .attr('y2', yScale(d.min) - yScale(d.median))
+        .style('stroke', 'black')
+        .style('stroke-width', '2px');
+      // line.max
+      d3.select(this)
+        .append("line")
+        .attr("class", "max")
+        .attr('x1', -10)
+        .attr('x2', 10)
+        .attr('y1', yScale(d.max) - yScale(d.median))
+        .attr('y2', yScale(d.max) - yScale(d.median))
+        .style('stroke', 'black')
+        .style('stroke-width', '2px');
+      // line.min
+      d3.select(this)
+        .append("line")
+        .attr("class", "min")
+        .attr('x1', -10)
+        .attr('x2', 10)
+        .attr('y1', yScale(d.min) - yScale(d.median))
+        .attr('y2', yScale(d.min) - yScale(d.median))
+        .style('stroke', 'black')
+        .style('stroke-width', '2px');
+      // rect.distribution
       d3.select(this)
         .append("rect")
+        .attr("class","distribution")
         .attr('width', '20')
         .attr('x', '-10')
         //per posizionare correttamente i box devo scalare dell'offset fra
         // mediana (centro del rettangolo) e margine superiore (terzo quartile)
         .attr('y', yScale(d.q3)-yScale(d.median))
         .attr('height', `${yScale(d.q1) - yScale(d.q3)}`)
-        .style('fill', 'transparent')
-        .style('stroke', 'black');
+        .style('fill', 'white')
+        .style('stroke', 'black')
+          .style('stroke-width', '2px');
+      // line.median
+      d3.select(this)
+        .append("line")
+        .attr("class", "median")
+        .attr('x1', -10)
+        .attr('x2', 10)
+        .style('stroke', 'darkgray')
+        .style('stroke-width', '4px');
     });
 }
