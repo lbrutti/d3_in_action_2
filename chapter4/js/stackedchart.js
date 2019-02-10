5719719df0e20e1ff692b8d8998bb3ef9d5020cb
Lorenzo Brutti <lorenzobrutti80@gmail.com>
2019-02-10 16:27:09 +0100

    proseguito stackedgraph

--- chapter4/js/stackedchart.js
+++ chapter4/js/stackedchart.js
@@ -2,9 +2,14 @@
   d3.csv("../data/movies.csv", lineChart);
 
   function lineChart(data) {
+    // debugger;
+    const fillScale = d3.scaleOrdinal()
+      .domain(["titanic", "avatar", "akira", "frozen", "deliverance", "avengers"])
+      .range(["#fcd88a", "#cf7c1c", "#93c464", "#75734F", "#5eafc6", "#41a368"]);
+
 
     const xScale = d3.scaleLinear().domain([1, 10]).range([20, 470]);
-    const yScale = d3.scaleLinear().domain([0, 40]).range([480, 20]);
+    const yScale = d3.scaleLinear().domain([0, 55]).range([480, 20]);
 
     const xAxis = d3.axisBottom()
       .scale(xScale)
@@ -22,18 +27,40 @@
 
     Object.keys(data[0]).forEach(key => {
       if (key !== "day") {
-        let movieArea = d3.line()
+
+        let movieLine = d3.line()
           .x(d => xScale(d.day))
           .y(d => yScale(d[key]))
-          .curve(d3.curveCardinal);
+          .curve(d3.curveBasis);
+
+        //impila i grafici l'uno sull'altro
+        const simpleStacking = (lineData, lineKey) => {
+          var newHeight = 0;
+          Object.keys(lineData).every(key => {
+            if (key !== "day") {
+              newHeight += parseInt(lineData[key]);
+              if (key === lineKey) {
+                return false;
+              }
+            }
+            return true;
+          });
+          return newHeight;
+        }
+
+        let movieArea = d3.area()
+          .x(d => xScale(d.day))
+          .y0(d => yScale(simpleStacking(d, key) - d[key])) //corrisponde ad accessore y di line
+          .y1(d => yScale(simpleStacking(d, key))) //indica il lower bound del grafico
+          .curve(d3.curveBasis);
 
         d3.select("svg")
           .append("path")
           .attr("id", `${key}Area`)
           .attr("d", movieArea(data))
           .attr('stroke', '#000000')
-          .attr('fill', '#ff0301')
-          .attr('opacity', .5);
+          .attr('fill', fillScale(key))
+          .attr('opacity', .75);
       }
     });
   }
