bd3c3dffef3a10204dd11575fb52bc8a3ea02b28
ekr - lorenzo.brutti <lorenzo.brutti@ekr.it>
2019-03-02 17:26:29 +0100

    inserito file js per piechart

---
+++ chapter5/js/pie.js
@@ -0,0 +1,40 @@
+/* jshint esversion: 6*/
+d3.json("../data/tweets.json", pieChart);
+
+function pieChart(data) {
+  var nestedTweets = d3.nest()
+    .key(d => d.user)
+    .entries(data.tweets);
+
+  nestedTweets.forEach(d => {
+    d.numTweets = d.values.length;
+    d.numFavorites = d3.sum(d.values, p => p.favorites.length);
+    d.numRetweets = d3.sum(d.values, p => p.retweets.length);
+  });
+
+  var pieChart = d3.pie();
+
+  var newArc = d3.arc();
+  newArc
+    .innerRadius(20)
+    .outerRadius(100);
+
+  pieChart.value(d => d.numTweets);
+
+  var yourPie = pieChart(nestedTweets);
+
+  var fillScale = d3.scaleOrdinal()
+    .range(["#fcd88a", "#cf7c1c", "#93c464", "#75734F"]);
+
+  d3.select("svg")
+    .append("g")
+      .attr("transform","translate(250,250)")
+    .selectAll("path")
+    .data(yourPie)
+    .enter()
+    .append("path")
+      .attr("d", newArc)
+      .style("fill", (d,i) => fillScale(i))
+      .style("stroke", "black")
+      .style("stroke-width", "2px");
+}
